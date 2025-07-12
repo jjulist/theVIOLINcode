d18a9899e8dbab56df860260b6e23e661316b6a7
jjulist <julist@gmail.com>
2025-07-12 08:34:11 +0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,14 @@
 /**
  * Utility functions for the application
+ * Includes className merging and other common utilities
  */
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 /**
- * Combines class names with Tailwind CSS merge functionality
+ * Combines className values and merges Tailwind classes
+ * @param inputs - Array of class values to merge
+ * @returns Merged className string
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
